93036455129c3428878e76a163d1b8c5b8657e19
PiwkaNorbert <71353104+PiwkaNorbert@users.noreply.github.com>
2022-05-29 19:59:38 +0200

    fixed even more

--- script.js
+++ script.js
@@ -1,57 +1,65 @@
-'use strict';
+"use strict";
+
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+let totalScore = 20;
+let highScore = 0;
+
+const number = document.querySelector(".number");
+const check = document.querySelector(".check");
+const guess = document.querySelector(".guess");
+const score = document.querySelector(".score");
+const again = document.querySelector(".again");
+const highscore = document.querySelector(".highscore");
+const body = document.querySelector("body");
 
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 };
-const displayScore = function (score) {
-  document.querySelector('.score').textContent = score;
+const displayScore = function (totalScore) {
+  document.querySelector(".score").textContent = totalScore;
 };
 
-let number = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
+again.addEventListener("click", function () {
+  totalScore = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  displayMessage("Start guessing...");
+  displayScore(score);
+  guess.value = "";
+  body.classList.remove("win-background");
+  number.classList.remove("win-number");
+  number.textContent = "?";
+});
 
-  if (!guess) {
-    displayMessage('No Number! Please Enter a number between 1 and 20');
+check.addEventListener("click", function () {
+  const guessed = Number(guess.value);
+  if (!guessed) {
+    displayMessage("No Number! Please Enter a number between 1 and 20");
 
     // When numbers aren't in the spectrum
-  } else if (guess > 20 || guess < 0) {
-    displayMessage('Please enter a number between 1 and 20');
+  } else if (guessed > 20 || guessed < 0) {
+    displayMessage("Please enter a number between 1 and 20");
 
-    //When player wins
-  } else if (guess === number) {
-    displayMessage('🎉 Correct number!');
-    document.querySelector('.number').textContent = number;
-    document.querySelector('body').style.backgroundColor = '#3f792d96';
-    document.querySelector('.number').style.width = '30rem';
-    if (highscore < score) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+    //When a player wins
+  } else if (guessed === randomNumber) {
+    displayMessage("🎉 Correct number!");
+    number.textContent = randomNumber;
+    body.classList.add("win-background");
+    number.classList.add("win-number");
+    if (highScore < totalScore) {
+      highScore = totalScore;
+      highScore.textContent = highScore;
     }
     //When guess is wrong
-  } else if (guess !== number) {
-    if (score > 1) {
-      displayMessage(guess > number ? `Too high` : `Too low`);
-      score--;
-      displayScore(score);
+  } else if (guessed !== randomNumber) {
+    if (totalScore > 1) {
+      displayMessage(guessed > randomNumber ? `Too high` : `Too low`);
+      totalScore--;
+      displayScore(totalScore);
     } else {
-      displayMessage('You lost!');
+      displayMessage("You lost!");
       displayScore(0);
     }
   }
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  number = Math.trunc(Math.random() * 20) + 1;
-
-  displayMessage('Start guessing...');
-  displayScore(score);
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#405252d2';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').textContent = '?';
 });
